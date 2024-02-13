4e95b4b0c9512b0ecbad567d3380ea34c110016c
피수영 <vltndud123@naver.com>
2024-02-13 22:14:21 +0900

    Modify binary host link.

--- rust_bench.rb
+++ rust_bench.rb
@@ -3,7 +3,7 @@
 class RustBench < Formula
   desc "It's stress test tool made by Rust"
   homepage "https://github.com/auburn0820/rust-bench"
-  url "https://github.com/auburn0820/rust-bench/releases/download/1.0.0/rust-bench"
+  url "https://github.com/auburn0820/rust-bench/releases/download/1.0.0/rb"
   sha256 "19f11deaaf2fe61935b15ef74f00415ea32c98bee8698c1eaa7bf1f0220a50e5"
 
   def install
